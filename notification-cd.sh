7f27111d2b5bb6c82ef022b7176b26b18ad7a27a
Gurpreet Singh <49033319+supersaiyane@users.noreply.github.com>
2023-10-04 09:39:27 +0530

    Update PwaApp-cd.sh

--- notification-cd.sh
+++ notification-cd.sh
@@ -1,20 +1,20 @@
 #check the current image tag version from Docker-compose file
-old_image_string="$(grep -P \^\.\*<account_id>.dkr.ecr.us-east-2.amazonaws.com/notificationsvc\.\*\$ /home/ubuntu/Deployment/docker-compose.yaml)"
+old_image_string="$(grep -P \^\.\*<account_id>.dkr.ecr.us-east-2.amazonaws.com/PwaApp\.\*\$ /home/ubuntu/Deployment/docker-compose.yaml)"
 echo $old_image_string
 old_image_tag=${old_image_string##*:}
 echo $old_image_tag
 
 #check the current image tag version from Docker-compose file
-latest_image_string="$(grep -P \^\.\*<account_id>.dkr.ecr.us-east-2.amazonaws.com/notificationsvc\.\*\$ /home/ubuntu/Services/notificationsvc/imagedefinitions.json)"
+latest_image_string="$(grep -P \^\.\*<account_id>.dkr.ecr.us-east-2.amazonaws.com/PwaApp\.\*\$ /home/ubuntu/Services/PwaApp/imagedefinitions.json)"
 echo $latest_image_string
 latest_image_tag=$(echo ${latest_image_string##*:} | tr -dc '0-9')
 echo $latest_image_tag
 
 #Replacing the latest tag with old tag
-sed -i 's/notificationsvc:'$old_image_tag'/notificationsvc:'$latest_image_tag'/g' /home/ubuntu/Deployment/docker-compose.yaml
+sed -i 's/PwaApp:'$old_image_tag'/PwaApp:'$latest_image_tag'/g' /home/ubuntu/Deployment/docker-compose.yaml
 
-#for verification purpose-notificationsvc
-image_number_2="$(grep -P \^\.\*<account_id>.dkr.ecr.us-east-2.amazonaws.com/notificationsvc\.\*\$ /home/ubuntu/Deployment/docker-compose.yaml)"
+#for verification purpose-PwaApp
+image_number_2="$(grep -P \^\.\*<account_id>.dkr.ecr.us-east-2.amazonaws.com/PwaApp\.\*\$ /home/ubuntu/Deployment/docker-compose.yaml)"
 echo $image_number_2
 
 #executing docker-compose commands and login ECR
